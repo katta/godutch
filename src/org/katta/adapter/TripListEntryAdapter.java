7ddc2e4d9efc8e67d79551780223781130bc8ff8
Srivatsa Katta <vatsa.katta@gmail.com>
2011-06-17 22:00:16 +0530

    new eclipse project created

---
+++ src/org/katta/adapter/TripListEntryAdapter.java
@@ -0,0 +1,65 @@
+package org.katta.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.katta.R;
+
+public class TripListEntryAdapter extends BaseAdapter {
+
+	private LayoutInflater layoutInflater;
+	private List<String> trips = new ArrayList<String>();
+
+	public TripListEntryAdapter(Context context) {
+		layoutInflater = LayoutInflater.from(context);
+
+		for (int i = 0; i < 100; i++) {
+			trips.add("Trip - " + i);
+		}
+	}
+
+	public int getCount() {
+		return trips.size();
+	}
+
+	public Object getItem(int i) {
+		return null;
+	}
+
+	public long getItemId(int i) {
+		return 0;
+	}
+
+	public View getView(int i, View convertView, ViewGroup parent) {
+
+		TripListEntryViewHolder viewHolder;
+
+		if (convertView == null) {
+			convertView = layoutInflater.inflate(R.layout.trip_list_entry,
+					parent, false);
+			viewHolder = new TripListEntryViewHolder();
+			viewHolder.checkbox = (CheckBox) convertView.findViewById(R.id.triplistentrycheckbox);
+			viewHolder.textview = (TextView) convertView.findViewById(R.id.triplistentrytext);
+			convertView.setTag(viewHolder);
+		} else {
+			viewHolder = (TripListEntryViewHolder) convertView.getTag();
+		}
+
+		viewHolder.textview.setText(trips.get(i));
+		return convertView;
+
+	}
+
+	static class TripListEntryViewHolder {
+		CheckBox checkbox;
+		TextView textview;
+	}
+}
